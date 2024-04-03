443a378baf8cca58ba48f08343954c96360c9e63
nafraus <133620859+nafraus@users.noreply.github.com>
2024-04-03 15:59:36 -0400

    Add files via upload

---
+++ VRHapticsManager-2.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class VRHapticsManager : MonoBehaviour
+{
+    public static VRHapticsManager Instance;
+
+    [SerializeField] private XRDirectInteractor leftInteractor;
+    [SerializeField] private XRDirectInteractor rightInteractor;
+    private void Awake()
+    {
+        if (Instance) Destroy(gameObject);
+        else Instance = this;
+    }
+
+    public void SendHapticImpulse(float amplitude, float duration, XRHapticControllerSpecifier type)
+    {
+        switch (type)
+        {
+            case XRHapticControllerSpecifier.left:
+                leftInteractor.SendHapticImpulse(amplitude, duration);
+                break;
+            case XRHapticControllerSpecifier.right:
+                rightInteractor.SendHapticImpulse(amplitude, duration);
+                break;
+            case XRHapticControllerSpecifier.both:
+                leftInteractor.SendHapticImpulse(amplitude, duration);
+                rightInteractor.SendHapticImpulse(amplitude, duration);
+                break;
+        }
+    }
+
+    public void SendHapticImpulse(float amplitude, float duration, XRDirectInteractor controller)
+    {
+        controller.SendHapticImpulse(amplitude, duration);
+    }
+
+    public void SendHapticImpulse(float amplitude, float duration, XRBaseController controller)
+    {
+        controller.SendHapticImpulse(amplitude, duration);
+    }
+
+    public void SendHapticImpulse(float amplitude, float duration, XRBaseInteractable interactable)
+    {
+        foreach(IXRSelectInteractor interactor in interactable.interactorsSelecting)
+        {
+            interactor.transform.GetComponent<XRDirectInteractor>()?.SendHapticImpulse(amplitude, duration);
+        }
+    }
+}
+
+public enum XRHapticControllerSpecifier
+{
+    left,
+    right,
+    both
+}
+
+
